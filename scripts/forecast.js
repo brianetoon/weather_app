1f6bd11e928bdab2fc93f74263649ea5c9d512a4
Brian <brian.elliot.toon@gmail.com>
2020-03-09 18:34:00 -0400

    get weather API call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,9 +1,19 @@
-// 2 requests to 2 different endpoints:
-// req to certain endpoint to get city information to get a city code
-// use city code to a weather conditions api to get weather in that area
-
 const key = 'uysmqUkOMhEE3CMH0AixP8rO14jGP9GT';
 
+// get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query =`${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+// get city information
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
@@ -12,12 +22,14 @@ const getCity = async (city) => {
     const response = await fetch(base + query); // returns promise
     const data = await response.json(); // returns promise
 
-    // console.log(data[0]);
     return data[0];
+};
 
-}
+getCity('ann arbor').then(data => {
+    return getWeather(data.Key);
+}).then(data => console.log(data))
+.catch(err => console.log(err));
 
-getCity('ann arbor')
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
-    
+// getWeather('329380')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
